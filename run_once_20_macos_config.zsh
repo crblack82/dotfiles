248879853507f73473f4836d9eb9bfc1b6f4edfb
Carson Black <crblack82@gmail.com>
2022-06-12 00:24:31 -0400

    Update killall

--- run_once_20_macos_config.zsh
+++ run_once_20_macos_config.zsh
@@ -121,9 +121,4 @@ defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 # Kill affected applications                                                  #
 ###############################################################################
 
-for app in
-	"Dock" \
-	"Finder" \
-	"iTerm2"; do
-	killall "${app}" &> /dev/null
-done
+for app in "Dock" "Finder" "iTerm2"; do killall "${app}" 2>/dev/null; done
