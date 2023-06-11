4383701c27376bcc087bbf312e6920da87b5e287
Carson Black <crblack82@gmail.com>
2023-06-11 01:07:14 -0400

    testing

--- home/setup/run_once_02_setup_brew.sh
+++ home/setup/run_once_02_setup_brew.sh
@@ -5,10 +5,10 @@ set -eufo pipefail
 echo "🍺 Installing homebrew bundle"
 
 # Dump existing  bundle to file
-brew bundle dump --force --global
+brew bundle dump --file ~/.Brewfile --force --global
 
 # Validate and install bundle
 HOMEBREW_BUNDLE_MAS_SKIP=TestFlight brew bundle check --file ~/.Brewfile
 
 # Cleanup files
-brew bundle cleanup --force
+brew bundle cleanup --file ~/.Brewfile --force
