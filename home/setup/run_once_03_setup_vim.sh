b5cc4868802ba194391988b93d99c32bc2e37e24
Carson Black <crblack82@gmail.com>
2023-06-11 01:26:04 -0400

    testing

--- home/setup/run_once_03_setup_vim.sh
+++ home/setup/run_once_03_setup_vim.sh
@@ -5,6 +5,8 @@ set -eufo pipefail
 mkdir -p ~/.vim/pack/myplugins/start
 cd ~/.vim/pack/myplugins/start
 
+echo ~/.vim/pack/myplugins/start/syntastic
+
 # Installing Sensible plugin
 if [ ! -d "~/.vim/pack/myplugins/start/sensible" ]; then
 	git clone https://tpope.io/vim/sensible.git
