b906ce3fb5c895a65be256af2a8f8f9dbf2e6477
Carson Black <crblack82@gmail.com>
2023-06-11 01:23:02 -0400

    testing

--- home/setup/run_once_03_setup_vim.sh
+++ home/setup/run_once_03_setup_vim.sh
@@ -2,16 +2,16 @@
 
 set -eufo pipefail
 
-mkdir -p ~/.vim/pack/myplugins/start
-cd ~/.vim/pack/myplugins/start
+mkdir -p /Users/carson/.vim/pack/myplugins/start
+cd /Users/carson/.vim/pack/myplugins/start
 
 # Installing Sensible plugin
-if [ ! -d "~/.vim/pack/myplugins/start/sensible" ]; then
+if [ ! -d "/Users/carson/.vim/pack/myplugins/start/sensible" ]; then
   echo "Directory does not exist!"
 fi
 
 
 # Installing Syntastic plugin
-if [ ! -d "~/.vim/pack/myplugins/start/syntastic" ]; then
+if [ ! -d "/Users/carson/.vim/pack/myplugins/start/syntastic" ]; then
 	git clone https://github.com/vim-syntastic/syntastic.git
 fi
