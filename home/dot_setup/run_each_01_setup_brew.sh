c65cdfa0017817b4fb2cbecbeebae8025f50bb9e
Carson Black <crblack82@gmail.com>
2023-06-11 02:20:39 -0400

    testing

--- home/dot_setup/run_each_01_setup_brew.sh
+++ home/dot_setup/run_each_01_setup_brew.sh
@@ -4,8 +4,8 @@ set -eufo pipefail
 
 echo "🍺 Installing homebrew bundle"
 
-# Validate and install bundle
-HOMEBREW_BUNDLE_MAS_SKIP=TestFlight brew bundle check --file $HOME/.Brewfile
+# Install bundle
+HOMEBREW_BUNDLE_MAS_SKIP=TestFlight brew bundle install --file $HOME/.Brewfile
 
 # Cleanup files
 brew bundle cleanup --file $HOME/.Brewfile --force
