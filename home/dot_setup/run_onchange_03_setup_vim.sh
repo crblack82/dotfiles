aa566c1e58a5c48c0067d2c9c91479bb3d73fe2a
Carson Black <crblack82@gmail.com>
2023-06-11 22:07:36 -0400

    testing

--- home/dot_setup/run_onchange_03_setup_vim.sh
+++ home/dot_setup/run_onchange_03_setup_vim.sh
@@ -11,3 +11,13 @@ fi
 if [ ! -d "$HOME/.vim/pack/myplugins/start/syntastic" ]; then
 	git clone https://github.com/vim-syntastic/syntastic.git
 fi
+
+# Installing fzf plugin
+if [ ! -d "$HOME/.vim/pack/myplugins/start/fzf" ]; then
+	git clone https://github.com/junegunn/fzf.git
+fi
+
+# Installing fzf.vim plugin
+if [ ! -d "$HOME/.vim/pack/myplugins/start/fzf.vim" ]; then
+	git clone https://github.com/junegunn/fzf.vim.git
+fi
