1e17004c51f7fa14a7b751f14598983f3dfc450f
Carson Black <crblack82@gmail.com>
2023-06-12 07:21:07 -0400

    testing

--- home/dot_config/private_fish/private_config.fish
+++ home/dot_config/private_fish/private_config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# nvm integration
 function nvm
    bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
 end
@@ -10,8 +9,6 @@ end
 set -x NVM_DIR ~/.nvm
 nvm use default --silent
 
-# iterm2 integration
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-# fzf integration
 fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs --history=\cr --processes=\cp --variables=\cv
