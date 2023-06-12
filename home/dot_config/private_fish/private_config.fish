c3e56e15dad57330b3e53caa7e62d284f6f0c41a
Carson Black <crblack82@gmail.com>
2023-06-12 07:00:40 -0400

    testing

--- home/dot_config/private_fish/private_config.fish
+++ home/dot_config/private_fish/private_config.fish
@@ -17,10 +17,10 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 fzf_configure_bindings --directory=\cf --git_log=\cl --git_status=\cs --history=\cr --processes=\cp --variables=\cv
 
 # Aliases
-alias cat="ccat"
-alias dl="cd ~/Downloads"
-alias dt="cd ~/Desktop"
-alias dot="cd ~/Github/dotfiles"
-alias git="hub"
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias localip="ipconfig getifaddr en0"
+abbr -a cat "ccat"
+alias dl "cd ~/Downloads"
+alias dt "cd ~/Desktop"
+alias dot "cd ~/Github/dotfiles"
+alias git "hub"
+abbr -a ip "dig +short myip.opendns.com @resolver1.opendns.com"
+abbr -a localip "ipconfig getifaddr en0"
