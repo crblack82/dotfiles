10a7e89144dd16188bad8cae4e50795d2377a015
Carson Black <crblack82@gmail.com>
2023-06-17 00:02:57 -0400

    testing

--- home/dot_config/private_fish/functions/fish_greeting.fish
+++ home/dot_config/private_fish/functions/fish_greeting.fish
@@ -1,3 +1,4 @@
 function fish_greeting
+    _pure_check_for_new_release
     fish_logo brblue brcyan brgreen
 end
--- home/dot_config/private_fish/private_config.fish
+++ home/dot_config/private_fish/private_config.fish
@@ -2,14 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# nvm integration
-function nvm
-   bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
-end
-
-set -x NVM_DIR ~/.nvm
-nvm use default --silent
-
 # iterm2 integration
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
