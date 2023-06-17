d3a65cc24f35d5e265cea980a718c044cace9ce5
Carson Black <crblack82@gmail.com>
2023-06-17 00:16:43 -0400

    testing

--- home/dot_config/private_fish/private_config.fish
+++ home/dot_config/private_fish/private_config.fish
@@ -8,5 +8,10 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 # fzf integration
 fzf_configure_bindings --directory=\cf --git_log=\cL --git_status=\cS --history=\ch --processes=\cp --variables=\cv
 
+# nvm integration
+function nvm
+    bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
+end
+
 # pyenv integration
 pyenv init - | source
