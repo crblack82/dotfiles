9d82300e6e7e437385c3973f2502060ddb7f6c14
Carson Black <crblack82@gmail.com>
2023-06-18 10:27:03 -0400

    testing

--- home/.chezmoiscripts/run_onchange_03_setup_iterm2.sh
+++ home/.chezmoiscripts/run_onchange_03_setup_iterm2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-set autolaunch_dir "/Users/$whoami/Library/Application\ Support/iTerm2/Scripts/AutoLaunch/"
+set autolaunch_dir "/Users/$USER/Library/Application\ Support/iTerm2/Scripts/AutoLaunch/"
 mkdir -p $autolaunch_dir
 
 cp ../dot_iterm2/AutoSwitchTheme.py $autolaunch_dir 
