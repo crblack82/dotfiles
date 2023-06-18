d1cbcfffa2802e4792efc514db02fc75bbfb8a0b
Carson Black <crblack82@gmail.com>
2023-06-18 12:23:00 -0400

    testing

--- install.sh
+++ install.sh
@@ -8,14 +8,15 @@ read -p "🤚 This script will setup chezmoi configure dot files. Press [Enter] 
 if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/.local/bin"
   chezmoi="$bin_dir/chezmoi"
-  if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
-  elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
-  else
-    echo "To install chezmoi, you must have curl or wget installed." >&2
-    exit 1
-  fi
+  echo $bin_dir
+  # if [ "$(command -v curl)" ]; then
+  #   sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+  # elif [ "$(command -v wget)" ]; then
+  #   sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+  # else
+  #   echo "To install chezmoi, you must have curl or wget installed." >&2
+  #   exit 1
+  # fi
 else
   chezmoi=chezmoi
 fi
