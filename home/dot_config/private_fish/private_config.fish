78c3968b6ce6b0897e7ca919a2e9bcf97032caf6
Carson Black <crblack82@gmail.com>
2023-06-18 14:22:18 -0400

    testing

--- home/dot_config/private_fish/private_config.fish
+++ home/dot_config/private_fish/private_config.fish
@@ -9,10 +9,10 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 fzf_configure_bindings --directory=\cf --git_log=\cL --git_status=\cS --history=\ch --processes=\cp --variables=\cv
 
 # nvm integration
+set -x NVM_DIR ~/.nvm
 function nvm
    bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
 end
-set -x NVM_DIR ~/.nvm
 nvm use default --silent
 
 # pyenv integration
