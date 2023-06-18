3366110f005fb75165f6ebe20c6a9dbf92763378
Carson Black <crblack82@gmail.com>
2023-06-17 20:50:26 -0400

    testing

---
+++ home/dot_setup/run_onchange_05_setup_iterm2.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+autolaunch_dir = /Users/${whoami}/Library/Application\ Support/iTerm2/Scripts/AutoLaunch/
+mkdir -p $autolaunch_dir
+
+cp ../dot_iterm2/AutoSwitchTheme.py $autolaunch_dir 
