2e81ccf15fcacafb058a25475add16204c89d6c2
Carson Black <crblack82@gmail.com>
2023-06-18 12:10:17 -0400

    testing

--- install.sh
+++ install.sh
@@ -20,6 +20,8 @@ else
   chezmoi=chezmoi
 fi
 
+echo 'help'
+
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 # exec: replace current process with chezmoi init
