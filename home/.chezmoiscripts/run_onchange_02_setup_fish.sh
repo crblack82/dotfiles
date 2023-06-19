922b8605e05163fce49f6cb1a273e7a463fb851d
Carson Black <crblack82@gmail.com>
2023-06-19 14:54:45 -0400

    testing

--- home/.chezmoiscripts/run_onchange_02_setup_fish.sh
+++ home/.chezmoiscripts/run_onchange_02_setup_fish.sh
@@ -11,17 +11,3 @@ fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/opt/curl/bin $fish_user_paths
 fish_add_path /opt/homebrew/opt/coreutils/libexec/gnubin $fish_user_paths
 fish_add_path /Users/$USER/Library/Application Support/JetBrains/Toolbox/scripts
-
-# ALIAS
-abbr cat "ccat"
-alias -s cleanup "find . -type f -name '*.DS_Store' -ls -delete"
-alias -s dl "cd ~/Downloads"
-alias -s dt "cd ~/Desktop"
-alias -s dot "cd ~/Github/dotfiles"
-alias -s ip "dig +short myip.opendns.com @resolver1.opendns.com"
-alias -s lss "ls -Gal"
-alias -s lp "ls -p"
-alias -s localip "ipconfig getifaddr en0"
-alias -s map "xargs -n1"
-alias -s vim "nvim"
-alias -s vi "nvim"
