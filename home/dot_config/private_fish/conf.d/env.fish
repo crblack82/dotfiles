7d72fc2557ed7c2c6449f4751d6a75d7a7e56c78
Carson Black <crblack82@gmail.com>
2023-06-19 15:07:29 -0400

    testing

---
+++ home/dot_config/private_fish/conf.d/env.fish
@@ -0,0 +1,4 @@
+set -Ux EDITOR nvim
+set -Ux JAVA_HOME /Library/Java/JavaVirtualMachines/temurin-20.jdk/Contents/Home
+set -Ux HOMEBREW_NO_ENV_HINTS true
+set -Ux JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION true
