eba46bdbb4a584a324dae6ab1b76652816fcb274
Carson Black <crblack82@gmail.com>
2023-06-18 22:31:34 -0400

    testing

--- home/.chezmoiscripts/run_onchange_02_setup_fish.sh
+++ home/.chezmoiscripts/run_onchange_02_setup_fish.sh
@@ -4,6 +4,7 @@
 set -Ux EDITOR nvim
 set -Ux JAVA_HOME /Library/Java/JavaVirtualMachines/temurin-20.jdk/Contents/Home
 set -Ux HOMEBREW_NO_ENV_HINTS true
+set -Ux JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION true
 
 # PATH
 fish_add_path /opt/homebrew/bin
