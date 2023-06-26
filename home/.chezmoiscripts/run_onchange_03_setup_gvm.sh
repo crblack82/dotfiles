586752bf597152b2d0d49f869613a6e6f8bd0970
Carson Black <crblack82@gmail.com>
2023-06-25 23:01:42 -0400

    testing

--- home/.chezmoiscripts/run_onchange_03_setup_gvm.sh
+++ home/.chezmoiscripts/run_onchange_03_setup_gvm.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env fish
 
 if ! test -d ~/.gvm
-    git clone https://github.com/nvim-lua/kickstart.nvim.git ~/.config/nvim
 	bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 end
