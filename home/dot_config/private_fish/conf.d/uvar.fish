9ed990d3acb76e0a5d966c13e041479196715b6e
Carson Black <crblack82@gmail.com>
2024-04-09 21:15:22 -0400

    Update java

--- home/dot_config/private_fish/conf.d/uvar.fish
+++ home/dot_config/private_fish/conf.d/uvar.fish
@@ -2,7 +2,7 @@ set -q MY_UVAR_SET; and return
 
 set -Ux EDITOR nvim
 set -Ux HOMEBREW_NO_ENV_HINTS true
-set -Ux JAVA_HOME /Library/Java/JavaVirtualMachines/temurin-20.jdk/Contents/Home
+set -Ux JAVA_HOME /Library/Java/JavaVirtualMachines/temurin-22.jdk/Contents/Home
 set -Ux JSII_SILENCE_WARNING_UNTESTED_NODE_VERSION true
 
 set -g MY_UVAR_SET true
